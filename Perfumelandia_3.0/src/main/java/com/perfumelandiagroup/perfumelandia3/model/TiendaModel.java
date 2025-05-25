ddd2bb51352c631e525723e64bdec63e729a691a
psmaicol <mi.giraldo@duocuc.cl>
2025-05-25 00:52:32 -0400

    se agrega el @Entity en la TiendaModel

--- Perfumelandia_3.0/src/main/java/com/perfumelandiagroup/perfumelandia3/model/TiendaModel.java
+++ Perfumelandia_3.0/src/main/java/com/perfumelandiagroup/perfumelandia3/model/TiendaModel.java
@@ -1,9 +1,10 @@
 package com.perfumelandiagroup.perfumelandia3.model;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
