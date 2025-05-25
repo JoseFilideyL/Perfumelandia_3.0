75ca8524e3a965bf04be5e804b334892c54478c3
psmaicol <mi.giraldo@duocuc.cl>
2025-05-25 00:57:45 -0400

    tabla completa

--- Perfumelandia_3.0/src/main/java/com/perfumelandiagroup/perfumelandia3/model/TiendaModel.java
+++ Perfumelandia_3.0/src/main/java/com/perfumelandiagroup/perfumelandia3/model/TiendaModel.java
@@ -1,6 +1,5 @@
 package com.perfumelandiagroup.perfumelandia3.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -10,6 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table (name = "tienda")
 public class TiendaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String nombre;
     private String direccion;
