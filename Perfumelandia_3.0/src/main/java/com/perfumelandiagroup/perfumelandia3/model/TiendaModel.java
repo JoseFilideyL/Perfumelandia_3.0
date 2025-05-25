688d9cf0636ffeccfd82814d979725f572c8fd1d
psmaicol <mi.giraldo@duocuc.cl>
2025-05-25 01:46:16 -0400

    cambio de codigo

--- Perfumelandia_3.0/src/main/java/com/perfumelandiagroup/perfumelandia3/model/TiendaModel.java
+++ Perfumelandia_3.0/src/main/java/com/perfumelandiagroup/perfumelandia3/model/TiendaModel.java
@@ -3,23 +3,22 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table (name = "tienda")
 public class TiendaModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_tienda")
+
+
     private String id;
-    @Column(name = "nombre_tienda")
+
     private String nombre;
-    @Column(name = "direccion_tienda")
+
     private String direccion;
-    @Column(name = "telefono_tienda")
+
     private int telefono;
-    @Column(name = "email_tienda")
+
     private String email;
 
 }
