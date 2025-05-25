bce10b0b7e29b0ea52092e9a6df3703068646125
psmaicol <mi.giraldo@duocuc.cl>
2025-05-25 00:23:00 -0400

    se agrega le @tabla para el oracle

--- Perfumelandia_3.0/src/main/java/com/perfumelandiagroup/perfumelandia3/model/TiendaModel.java
+++ Perfumelandia_3.0/src/main/java/com/perfumelandiagroup/perfumelandia3/model/TiendaModel.java
@@ -1,4 +1,5 @@
 package com.perfumelandiagroup.perfumelandia3.model;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -6,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Table (name = "tienda")
 public class TiendaModel {
     private String id;
     private String nombre;
